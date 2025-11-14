5d7358e4a5624c1eb6745109241af28a602015a7
haytameel <haytameelharhari@gmail.com>
2025-11-14 17:17:34 +0100

    test(it01-h01): Test aceptación h01 realizado y ejecutado

--- tests/it-01/acceptance/at-h02.test.js
+++ tests/it-01/acceptance/at-h02.test.js
@@ -1,27 +1,29 @@
 import {UserService} from "../../../src/domain/service/UserService";
 import { describe, it, expect, beforeAll, beforeEach, afterAll, afterEach,test } from 'vitest';
+import {UserSession} from "../../../src/domain/session/UserSession";
 
 let userService;
 beforeAll(async () => {
     userService = UserService.getInstance();
 });
 beforeEach(async () => {
-  await userService.signUp("al123456@uji.es", "Maria", "MiContrasena64");
+//  await userService.signUp("al123456@uji.es", "Maria", "MiContrasena64");
 });
 
 describe("HU02 - Inicio de sesión", () => {
   test("E1 - Válido: inicia sesión correctamente", async () => {
     const result = await userService.logIn("al123456@uji.es", "MiContrasena64");
-    expect(result.sessionOpen).toBe(true);
+    expect(result.sessionOpen).toBe(typeof UserSession);
+    userService.logOut();
   });
 
   test("E2 - Inválido: contraseña incorrecta", async () => {
     await expect(userService.logIn("al123456@uji.es", "micontraseña"))
-      .rejects.toThrow("InvalidDataException");
+      .rejects.toThrow("InvalidCredentials");
   });
 
   test("E2b - Inválido: correo no registrado", async () => {
     await expect(userService.logIn("al987654@uji.es", "Codigo1Secreto2"))
-      .rejects.toThrow("EmailNotFoundException");
+      .rejects.toThrow("UserNotFound");
   });
 });
